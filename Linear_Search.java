2a7dfbaf323495521e280f4b36ecb84dcce3f282
indicgenous <sid07102002@gmail.com>
2022-03-21 12:04:01 +0530

    Updated

--- Linear_Search.java
+++ Linear_Search.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Linear_Search {
     public static void main(String[] args) {
-        int[]arr={2,34,6,3,1,7,8,-1};
-        int target=3;
-        int k =lin_search(arr,target);
-        System.out.println(k);
+        int[][]arr={{2,34,6},{4,5,43},{56,78,90,92,99}};
+        int target = 34;
+        int[] k =lin2d(arr,target);
+        System.out.println(Arrays.toString(k));
 
     }
     static int lin_search(int[]arr,int target){
@@ -16,5 +18,18 @@ public class Linear_Search {
         // this line will execute if none of the return statements above have executed
         // hence the target not found
         return -1;
+    }
+//    Lin search in a 2d matrix
+    static int[] lin2d(int[][]arr, int target){
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr[i].length; j++) {
+                if (target == arr[i][j]) {
+                    return new int[]{i,j};
+                }
+
+            }
+        //this line will execute if none of the return statements above have executed.
+        // the target not found.
+        return new int[]{-1,-1};
     }
 }
